5371267c62ece3c61df6bd52a9d1b26d57709455
Jack Heidal <jpheidal@gmail.com>
2022-01-06 20:17:41 -0800

    Completed day 6

---
+++ day6.py
@@ -0,0 +1,21 @@
+def readinput(file):
+    f = open(file, 'r')
+    data = f.readline()
+    f.close()
+    numbers = [data.count(str(n)) for n in range(9)]
+    return numbers
+
+
+def main(fishdist):
+    num = fishdist
+    for _ in range(256):
+        newnum = num[1:]
+        newnum[6] += num[0]
+        newnum.append(num[0])
+        num = newnum
+    print(sum(num))
+
+
+if __name__ == '__main__':
+    num = readinput('inputs/D6.txt')
+    main(num)
