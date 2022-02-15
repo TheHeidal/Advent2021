3ffaf06fb6c82d9a09556ed31c37e2d8408ed975
Jack Heidal <jpheidal@gmail.com>
2022-02-15 14:07:03 -0800

    Optimized rotation and rebasing generation, still only passing test case

--- day19.py
+++ day19.py
@@ -42,27 +42,29 @@ class Scanner:
     def __init__(self, index, clouds):
         self.index = index  # index of the scanner's cloud in scanner_clouds
         self.uncompared = set(range(len(clouds))) - {index}
-        self.base_cloud = clouds[index]
+        self.u_cloud = clouds[index]
         if index == 0:
             self.location = np.array([0, 0, 0])
-            self.solved_cloud = clouds[index]
+            self.s_cloud = clouds[index]
             self.rotation_i = IDENTITY_ROT_I
             self.isSolved = True
         else:
-            self.location = None
             self.isSolved = False
+            self.location = None
+            self.solved_from = None
             self.rotation_i = None
-            self.solved_cloud = None
+            self.s_cloud = None
 
-    def solve(self, solved_scanner_i, solved_beacon_i, self_beacon_i, rotation_i):
+    def solve(self, s_scanner_i, solved_beacon_i, self_beacon_i, rotation_i):
         self.isSolved = True
+        self.solved_from = s_scanner_i
         self.rotation_i = rotation_i
 
-        rotated_cloud = np.einsum('yx,zx->zy', ROTATIONS[rotation_i], self.base_cloud)
-        s_beacon_coord = scn_dict[solved_scanner_i].solved_cloud[solved_beacon_i]
+        rotated_cloud = np.einsum('yx,zx->zy', ROTATIONS[rotation_i], self.u_cloud)
+        s_beacon_coord = scn_dict[s_scanner_i].s_cloud[solved_beacon_i]
         offset = s_beacon_coord - rotated_cloud[self_beacon_i]
-        self.solved_cloud = rotated_cloud + offset
-        self.location = scn_dict[solved_scanner_i].location + offset
+        self.s_cloud = rotated_cloud + offset
+        self.location = scn_dict[s_scanner_i].location + offset
         logging.info(f"solved scanner {self.index}!")
 
     def __repr__(self):
@@ -90,32 +92,20 @@ ROTATIONS = np.array([xr[x % 4] @ yr[y % 4] @ zr[z % 4]
 # x is right, y is forwards, z is up. x and z follow left hand rule, y follows right (should probably fix that)
 
 def find_matches():
-    # pick a solved scanner
-    # for each beacon b (until we are sure we must have tried one in the overlap cloud):
-    #   get the coordinates of every beacon in the cloud relative to b (rebase onto b)
-    #   for each scanner we haven't compared the solved scanner to (the comparison scanner)
-    #       for each beacon c_b in the comparison scanner's cloud
-    #           for each possible rotation r:
-    #               rebase all beacons onto c_b
-    #               rotate all beacons by r
-    #               if the rebased clouds have at least 12 beacons in common, we know
-    #                   - the comparison scanner can be corrected with rotation r
-    #                   - b and cb are the same beacon
-    #                   - true location of every beacon seen by comp_scanner is r@(beacon - cb) + origin beacon
     solved_scanners = [0]
-    for solved_scanner_i in solved_scanners:
-        while len(scn_dict[solved_scanner_i].uncompared):
-            unsolved_scanner_i = scn_dict[solved_scanner_i].uncompared.pop()
-            if not scn_dict[unsolved_scanner_i].isSolved:
-                logging.debug(f'comparing scanner {solved_scanner_i:2} to scanner {unsolved_scanner_i:2}')
-                res = match_scanners(solved_scanner_i, unsolved_scanner_i)
-                scn_dict[solved_scanner_i].uncompared.discard(unsolved_scanner_i)
-                scn_dict[unsolved_scanner_i].uncompared.discard(solved_scanner_i)
+    for s_scanner_i in solved_scanners:
+        while len(scn_dict[s_scanner_i].uncompared):
+            u_scanner_i = scn_dict[s_scanner_i].uncompared.pop()
+            if not scn_dict[u_scanner_i].isSolved:
+                logging.debug(f'comparing scanner {s_scanner_i:2} to scanner {u_scanner_i:2}')
+                res = match_scanners(s_scanner_i, u_scanner_i)
+                scn_dict[s_scanner_i].uncompared.discard(u_scanner_i)
+                scn_dict[u_scanner_i].uncompared.discard(s_scanner_i)
                 if res is not None:
                     logging.debug(f'overlap found!')
                     s_beacon_i, u_beacon_i, rot_i = res
-                    scn_dict[unsolved_scanner_i].solve(solved_scanner_i, s_beacon_i, u_beacon_i, rot_i)
-                    solved_scanners.append(unsolved_scanner_i)
+                    scn_dict[u_scanner_i].solve(s_scanner_i, s_beacon_i, u_beacon_i, rot_i)
+                    solved_scanners.append(u_scanner_i)
     return None
 
 
@@ -132,34 +122,37 @@ def match_scanners(s_scan_i, u_scan_i):
     #                   - the comparison scanner can be corrected with rotation r
     #                   - b and cb are the same beacon
     #                   - true location of every beacon seen by comp_scanner is r@(beacon - cb) + origin beacon
-    rotated_uns_beacons = np.einsum('wyx,zx->wzy', ROTATIONS, scn_dict[u_scan_i].base_cloud)
 
-    for s_beacon_i in range(len(scn_dict[s_scan_i].solved_cloud) - (OVERLAP_COUNT - 1)):
+    s_beacons = scn_dict[s_scan_i].s_cloud
+    reb_s_beacons = s_beacons - np.expand_dims(s_beacons, axis=1)
+    # axes are rebased beacon, beacon, vector components
+    rot_u_beacons = np.einsum('wyx,zx->wzy', ROTATIONS, scn_dict[u_scan_i].u_cloud)
+    # axes are rotation, beacon, vector components
+    reb_u_beacons = np.expand_dims(rot_u_beacons, axis=1) - np.expand_dims(rot_u_beacons, axis=2)
+    # axes are rotation, rebased beacon, beacon, vector components
+
+    for s_beacon_i in range(len(s_beacons)):
         # includes at least one scanner in the overlap cloud, if any exist
-        rebased_s_beacons = scn_dict[s_scan_i].solved_cloud - scn_dict[s_scan_i].solved_cloud[s_beacon_i]
-        s_view = rebased_s_beacons.view(view_dtype)
+        s_view = reb_s_beacons[s_beacon_i].view(view_dtype)
 
-        for uns_beacon_i in range(len(scn_dict[u_scan_i].base_cloud)):
-            rebased_uns_beacons = rotated_uns_beacons - rotated_uns_beacons[:, np.newaxis, uns_beacon_i]
-            for rotation_i in range(len(ROTATIONS)):
-                u_view = rebased_uns_beacons[rotation_i].view(view_dtype)
-                matches, s_indices, c_indices = np.intersect1d(s_view, u_view, return_indices=True)
+        for u_beacon_i in range(len(scn_dict[u_scan_i].u_cloud)):
+            for rot_i in range(len(ROTATIONS)):
+                u_view = reb_u_beacons[rot_i][u_beacon_i].view(view_dtype)
+                matches = np.intersect1d(s_view, u_view, assume_unique=True)
                 if matches.size >= 12:
-                    return s_beacon_i, uns_beacon_i, rotation_i
+                    return s_beacon_i, u_beacon_i, rot_i
 
 
 def count_beacons():
-    beacons = np.vstack([scn_dict[i].solved_cloud for i in range(len(scanner_clouds))])
+    beacons = np.vstack([scn_dict[i].s_cloud for i in range(len(scanner_clouds))])
     b_set = np.unique(beacons, axis=0)
     return b_set.shape[0]
 
 
 IDENTITY_ROT_I = 0
 OVERLAP_COUNT = 12
-view_dtype = {'names': ['f0', 'f1', 'f2'], 'formats': [int, int, int]}
+view_dtype = {'names': ['x', 'y', 'z'], 'formats': [int, int, int]}
 
-test_case = 'inputs/D19test.txt'
-real_case = 'inputs/D19.txt'
 
 if __name__ == '__main__':
     scanner_clouds = read_input('inputs/D19test.txt')
