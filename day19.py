aa1582effa09591cb3161f01235e57948b39d39d
Jack Heidal <jpheidal@gmail.com>
2022-03-03 13:02:13 -0800

    Rewrote to do distance matching

--- day19.py
+++ day19.py
@@ -1,8 +1,8 @@
 import logging
 import re
-import numpy as np
 from collections import deque, defaultdict
 
+import numpy as np
 
 logging.basicConfig(filename='day19.log',
                     encoding='utf-8',
@@ -14,7 +14,7 @@ logging.basicConfig(filename='day19.log',
 
 def read_input(filename):
     """Parses input files and returns a list of np arrays of beacon locations"""
-    scanner_name = re.compile(r'--- scanner (\d+) ---')
+    scanner_name = re.compile(r"--- scanner (\d+) ---")
     beacon_coord = re.compile(r'(-?\d*),(-?\d*),(-?\d*)')
 
     logging.info(f'reading {filename}')
@@ -27,18 +27,18 @@ def read_input(filename):
         elif line == '\n':
             scanner_lists.append(np.array(curr_ls))
         else:
-            obj = beacon_coord.match(line)
-            curr_ls.append([int(obj.group(1)), int(obj.group(2)), int(obj.group(3))])
+            b_match = beacon_coord.match(line)
+            curr_ls.append([int(b_match.group(1)), int(b_match.group(2)), int(b_match.group(3))])
     file.close()
     return scanner_lists
 
 
 OVERLAP_COUNT = 12
-NECESSARY_OVERLAP = (OVERLAP_COUNT * (OVERLAP_COUNT - 1)) // 2
+MIN_OVERLAP = (OVERLAP_COUNT * (OVERLAP_COUNT - 1)) // 2
 view_dtype = {'names': ['x', 'y', 'z'], 'formats': [int, int, int]}
 
 
-class ScannerDist:
+class Scanner:
     """A scanner that stores pairs of beacons by their distance
 
     Attributes:
@@ -73,70 +73,89 @@ class ScannerDist:
             self.s_cloud = self.cloud
             self.offset = np.zeros(3, dtype=np.int32)
         else:
-            self.s_cloud = None
+            self.parent = None
+            self.rotation = None
             self.offset = None
+            self.s_cloud = None
+
+    def solve(self, new_parent, new_rotation, new_offset):
+        assert self.parent is None
+        self.parent = new_parent
+        self.rotation = new_rotation
+        self.offset = new_offset
+        parent_cloud_view = self.parent.cloud.view(view_dtype)
+        offset_view = (rotate_vectors(self.cloud, u_scanner.rotation) + new_offset).view(view_dtype)
+        intersect_d = np.intersect1d(parent_cloud_view, offset_view)
+        assert OVERLAP_COUNT <= len(intersect_d)
 
     def __repr__(self):
-        return f"Distance Scanner {self.index}"
+        return f"Scanner {self.index}"
 
 
-def match_beacons(dist, scanner_i0, scanner_i1, scanner_list):
-    """Returns a pair of pairs of matched beacons (scanner_i0, scanner_i1)"""
+def match_beacons(scanner_x, scanner_y):
+    shared_dists = scanner_x.dist_to_pair.keys() & scanner_y.dist_to_pair.keys()
+    if len(shared_dists) >= MIN_OVERLAP:
+        logging.debug(f"overlap found btw s {scanner_x.index} and s {scanner_y.index}")
+        for dist in shared_dists:
+            sx_b0, sx_b1, _ = scanner_x.dist_to_pair[dist]
+            sy_b0, sy_b1, _ = scanner_y.dist_to_pair[dist]
 
-    scanner_0 = scanner_list[scanner_i0]
-    scanner_1 = scanner_list[scanner_i1]
-    s0b0, s0b1, _ = scanner_0.dist_to_pair[dist]
-    s1b0, s1b1, _ = scanner_1.dist_to_pair[dist]
+            def are_same_beacon(b1, b2):
+                return len(scanner_x.beacon_to_dists[b1] & scanner_y.beacon_to_dists[b2]) >= OVERLAP_COUNT - 1
 
-    def are_same_beacon(b1, b2):
-        return len(scanner_0.beacon_to_dists[b1] & scanner_1.beacon_to_dists[b2]) >= OVERLAP_COUNT - 1
+            if are_same_beacon(sx_b0, sy_b0):
+                return (sx_b0, sx_b1), (sy_b0, sy_b1)
+            elif are_same_beacon(sx_b0, sy_b1):
+                return (sx_b0, sx_b1,), (sy_b1, sy_b0)
+        return None
+    else:
+        return None
 
-    if are_same_beacon(s0b0, s1b0):
-        return (s0b0, s0b1), (s1b0, s1b1)
-    elif are_same_beacon(s0b0, s1b1):
-        return (s0b0, s0b1,), (s1b1, s1b0)
+
+def rotate_vectors(vectors, rotation_matrix):
+    return np.einsum("bc,rc->br", vectors, rotation_matrix)
 
 
 if __name__ == '__main__':
     scanner_clouds = read_input('inputs/D19test.txt')
     # scanner_clouds = read_input('inputs/D19.txt')
 
-    scan_lst = [ScannerDist(i, scanner_clouds[i]) for i in range(len(scanner_clouds))]
+    scan_lst = [Scanner(i, scanner_clouds[i]) for i in range(len(scanner_clouds))]
 
-    scanner_queue = deque()
-    scanner_queue.append(0)
-    u_scanners_set = {i for i in range(1, len(scan_lst))}
+    scanner_queue = deque([0])
+    u_scanners_set = set(range(len(scan_lst))) - {0}
 
     while scanner_queue:
         s_scan_i = scanner_queue.pop()
-        s_scanner = scan_lst[s_scan_i]
+        parent = scan_lst[s_scan_i]
         newly_solved = set()
         for u_scan_i in u_scanners_set:
             u_scanner = scan_lst[u_scan_i]
-            logging.debug(f"attempting to solve {u_scanner} with {s_scanner}")
-            dist_intersect = s_scanner.dist_to_pair.keys() & u_scanner.dist_to_pair.keys()
-            if len(dist_intersect) >= NECESSARY_OVERLAP:
-                logging.debug(f"overlap found")
-                for shared_dist in dist_intersect:
-                    break
-                s_beacons, u_beacons = match_beacons(shared_dist, s_scan_i, u_scan_i, scan_lst)
-                s_vector = s_scanner.s_cloud[s_beacons[0]] - s_scanner.s_cloud[s_beacons[1]]
-                assert 0 not in s_vector
+            logging.debug(f"attempting to solve {u_scanner} with {parent}")
+            matched_beacons = match_beacons(parent, u_scanner)
+            if matched_beacons is not None:
+                s_beacons, u_beacons = matched_beacons
+                s_vector = parent.cloud[s_beacons[0]] - parent.cloud[s_beacons[1]]
                 u_vector = u_scanner.cloud[u_beacons[0]] - u_scanner.cloud[u_beacons[1]]
+                assert 0 not in s_vector
                 assert 0 not in u_vector
+
                 rotation = np.array(
                     [[1 if s_vector[s] == u_vector[u] else (-1 if s_vector[s] == -u_vector[u] else 0) for s in range(3)]
-                     for u in range(3)])
-                offset = s_scanner.s_cloud[s_beacons[0]] - (u_scanner.cloud[u_beacons[0]] @ rotation)
-                u_scanner.s_cloud = np.einsum("bc,rc->br", u_scanner.cloud, rotation) + offset
-                assert OVERLAP_COUNT <= len(np.intersect1d(s_scanner.s_cloud.view(view_dtype),
-                                                           u_scanner.s_cloud.view(view_dtype)))
-                u_scanner.offset = offset
+                     for u in range(3)], dtype=np.int32)
+                assert np.array_equal(u_vector @ rotation, s_vector)
+
+                offset = parent.cloud[s_beacons[0]] - (u_scanner.cloud[u_beacons[0]] @ rotation)
+                assert np.array_equal(parent.cloud[s_beacons[1]], u_scanner.cloud[u_beacons[1]] @ rotation + offset)
+
+                u_scanner.solve(parent, rotation, offset)
+
+
                 newly_solved.add(u_scan_i)
-        scanner_queue.extend(newly_solved)
+                scanner_queue.extend(newly_solved)
         u_scanners_set.difference_update(newly_solved)
     assert not u_scanners_set
     for s in scan_lst:
         print(s.offset)
-    num_beacons = np.unique(np.vstack([s.s_cloud for s in scan_lst]).view(view_dtype))
-    print(len(num_beacons))
+        num_beacons = np.unique(np.vstack([s.s_cloud for s in scan_lst]).view(view_dtype))
+        print(len(num_beacons))
